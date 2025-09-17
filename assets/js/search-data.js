feff99b25cbccd65a67c22bda80b8075bb271e13
MichalTesnar <37748649+MichalTesnar@users.noreply.github.com>
2025-09-17 13:55:27 +0000

    Deploying to gh-pages from @ MichalTesnar/michaltesnar.github.io@0ca632e7a08727b461970448c3df136f6915f29b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,14 +25,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Selection of things that I did, and I am also proud of.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -320,7 +320,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/assets/pdf/resume.pdf";
           
         },
       },{id: "post-a-post-with-diagrams",
