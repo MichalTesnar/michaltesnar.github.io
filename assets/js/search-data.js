3eb3cd237bc8969e061e1d2c229074ac6e5e77f5
MichalTesnar <37748649+MichalTesnar@users.noreply.github.com>
2025-11-02 09:59:46 +0000

    Deploying to gh-pages from @ MichalTesnar/michaltesnar.github.io@6ff80906e8171d15968654d7110a0e7055845ac6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-polsko-není-tak-daleko-jak-by-se-člověku-zdálo",
+        },{id: "post-výlet-do-mnichova",
+        
+          title: "Výlet do Mnichova",
+        
+        description: "Návštěva u bývalého spolužáka",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/munich-trip/";
+          
+        },
+      },{id: "post-polsko-není-tak-daleko-jak-by-se-člověku-zdálo",
         
           title: "Polsko není tak daleko, jak by se člověku zdálo",
         
